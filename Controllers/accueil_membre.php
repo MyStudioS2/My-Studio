33a7b1fa05a003e5c85fc8db46e0228e49cc34e3
Sid-Athos <sa.bennaceur@gmail.com>
2018-06-03 18:57:20 +0200

    Accueil responsif mamène

---
+++ Controllers/accueil_membre.php
@@ -0,0 +1,21 @@
+<!DOCTYPE>
+    <?php session_start()?>
+<html>
+<head>
+
+    <title>MyStudio</title>
+    <meta content='http-equiv' content-type='text/html; charset = UTF-8'/>
+    <link href="https://fonts.googleapis.com/css?family=Titillium+Web" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css">
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js" type="text/javascript"></script>
+    <script src="" type="text/javascript"></script>
+
+</head>
+
+<body>
+    <?php include('navbar.php');?>
+    <h1>Test</h1>
+</body>
+
+</html>
