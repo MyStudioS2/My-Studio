dddcae4d8f216b67cee4cb96a22df0fd8c6a7de1
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-05 23:07:52 +0200

    oui monseigneur

--- Player/Models/insert_album.php
+++ Player/Models/insert_album.php
@@ -3,18 +3,23 @@
             id_album,
             album_name,
             album_cover,
-            release_date
+            release_date,
+            artist_id
         ) VALUES (
             :ID,
             :album_name,
             :cover_path,
-            :release_date)";
+            :release_date,
+            (SELECT id_artist 
+            FROM artists
+            WHERE artist_name LIKE :artist))";
         // Security measures
     $query_params = array(
         ':ID' => NULL,
         ':album_name' => $album_name,
         ':cover_path' => $cover_path,
-        ':release_date' =>$date);
+        ':release_date' =>$date,
+        ':artist' => $artist);
     try {
         $stmt = $db->prepare($query);
         $result = $stmt->execute($query_params);
