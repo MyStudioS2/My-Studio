df5150031b2486284ae38b3c9936e869d5b478cf
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-05 23:37:53 +0200

    lololo

--- Player/Controllers/upload_music.php
+++ Player/Controllers/upload_music.php
@@ -7,12 +7,10 @@ switch(isset($_POST)):
     case(isset($_POST['album'])):
     $album_name=$_POST['album'];
     $album = &$album_name;
-    echo $album;
     $album_date = $_POST['release_date'];
     $tracks_amount = intval($_POST['track_count']);
     $date = date_to_mysql($album_date);
     $artist = "Joey Bad4$$"; 
-
     /** Création du répertoire de l'album */
     if(!is_dir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist)){
         mkdir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'');
@@ -21,17 +19,14 @@ switch(isset($_POST)):
         mkdir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name.'');
     }
     $successmsg = 'Le dossier a bien été créé';
-
     /** Path pour l'upload */
-    $dir='C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name;
+    $dir='C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name.'/';
     $cover_path=$dir.basename($_FILES['coverFile']['name']);
     if(move_uploaded_file($_FILES['coverFile']['tmp_name'],$cover_path)){
         echo 'upload successfull<br>';
     }
-    
     include('../Models/insert_album.php');
     include("../Views/add_music_form.php");
-
     break;
     case(isset($_POST['upload'])):
         $i = 0;
@@ -42,16 +37,13 @@ switch(isset($_POST)):
             echo $title.$album.$feat.'<br>';
             $i++;
         }
-
         /** Path pour l'upload */
         $dir='C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album;
         $audio_path=$dir.basename($_FILES['audioFile']['name']);
         if(move_uploaded_file($_FILES['audioFile']['tmp_name'],$audio_path)){
             echo 'upload successfull<br>';
         }
-        
         /** Insertion des musiques dans la BDD */
-        
         include('../Models/insert_music.php');
         include('../Models/get_music.php');
         $row = $stmt->fetchAll();
