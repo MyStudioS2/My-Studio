b2c41497f3533f1d120e908bf1355e3696cc4716
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-06 02:38:53 +0200

    lol

--- Player/Views/choose_amount.php
+++ Player/Views/choose_amount.php
@@ -1,36 +1,52 @@
 <!doctype html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-  <link rel="stylesheet" href="/resources/demos/style.css">
-  <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet">
-  <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-  <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-  <style>
-    body {
-      font-family: 'Encode Sans Condensed', sans-serif;
-    }
-  </style>
-  <body>
-    <center><div class="container" style ="max-width: 250px; text-align:center">
-      <form action="../Controllers/upload_music.php" method="POST" name="album_choice" enctype="multipart/form-data">
-        Titre de l'album :<br>
-        <input type="text" style ="text-align:center" name="album" placeholder="Nom de l'album"><br>
-        Nombre de pistes :<br>
-        <input type="number" style ="width:35px " min ="1" max="30" step="1" name="track_count"value="1"><br>
-        Date :<br>
-        <input type="text" id="datepicker" style="width:70px" name="release_date"><br>
-        Cover : <br>
-        <input type='file' name ='coverFile' enctype="multipart/form-data"/>
-        <input type="submit" name="confirmer" value="Confirmer">
-      </form>
-    </div></center>
-  </body>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<link rel="stylesheet" href="/resources/demos/style.css">
+<link href="https://fonts.googleapis.com/css?family=Encode+Sans+Condensed" rel="stylesheet">
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<style>
+  body {
+    font-family: 'Encode Sans Condensed', sans-serif;
+  }
+</style>
+<body>
+  <center><div class="container" style ="max-width: 450px; text-align:center">
+    <form action="../Controllers/upload_music.php" method="POST" name="album_choice" enctype="multipart/form-data">
+      Titre de l'album :<br>
+      <input type="text" style ="text-align:center" name="album" placeholder="Nom de l'album"><br>
+      Nombre de pistes :<br>
+      <input type="number" style ="width:35px " min ="1" max="30" step="1" name="track_count"value="1"><br>
+      Date :<br>
+      <input type="text" id="datepicker" style="width:70px" name="release_date"><br>
+      Cover : <br>
+      <input type='file' name ='coverFile' onchange="readURL(this);" />
+      <img id="blah" style="max-width:190px; max-height:150px" alt="Preview" /><br>
+      <input type="submit" name="confirmer" value="Confirmer">
+    </form>
+  </div></center>
+</body>
 </html>
- <script>
-  $( function() {
-    $( "#datepicker" ).datepicker({maxDate: "-1"});
-   } );
- </script>
+<script>
+$( function() {
+  $( "#datepicker" ).datepicker({maxDate: "-1"});
+  } );
+</script>
+<script>
+  function readURL(input) {
+      if (input.files && input.files[0]) {
+          var reader = new FileReader();
+
+          reader.onload = function (e) {
+              $('#blah')
+                  .attr('src', e.target.result);
+          };
+
+          reader.readAsDataURL(input.files[0]);
+      }
+  }
+</script>
