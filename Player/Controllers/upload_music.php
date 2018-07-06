2a417c89ab38453f6365cf781da7248e51c5f546
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-06 13:00:27 +0200

    lol

--- Player/Controllers/upload_music.php
+++ Player/Controllers/upload_music.php
@@ -10,11 +10,11 @@ switch(isset($_POST)):
     $date = $album_date;
     $artist = "Joey Bad4$$"; 
     /** Création du répertoire de l'album */
-    if(!is_dir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist)){
-        mkdir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'');
+    if(!is_dir('../../../data/mystudio/musics/'.$artist)){
+        mkdir('../../../data/mystudio/musics/'.$artist.'');
     }
-    if(!is_dir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name.'')){
-        mkdir('C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name.'');
+    if(!is_dir('../../../data/mystudio/musics/'.$artist.'/'.$album_name.'')){
+        mkdir('../../../data/mystudio/musics/'.$artist.'/'.$album_name.'');
     } else {
         include('../Views/choose_amount.php');
         header("refresh:15;url=../Controllers/upload_music.php");
@@ -28,7 +28,7 @@ switch(isset($_POST)):
     }
     $successmsg = 'Le dossier a bien été créé';
     /** Path pour l'upload */
-    $dir='C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album_name.'/';
+    $dir='../../../data/mystudio/musics/'.$artist.'/'.$album_name.'/';
     $cover_path=$dir.basename($_FILES['coverFile']['name']);
     if(move_uploaded_file($_FILES['coverFile']['tmp_name'],$cover_path)){
     }
@@ -42,7 +42,7 @@ switch(isset($_POST)):
         while($i < count($_POST['title'])) {
             $title = htmlspecialchars(trim($_POST['title'][$i]), ENT_QUOTES, 'UTF-8');
             $feat = htmlspecialchars(trim($_POST['feat'][$i]), ENT_QUOTES, 'UTF-8');
-            $dir='C:/Code/bin/mysql/mysql5.7.21/data/mystudio/musics/'.$artist.'/'.$album.'/';
+            $dir='../../../data/mystudio/musics/'.$artist.'/'.$album.'/';
             $audio_path=$dir.basename($_FILES['audioFile']['name'][$i]);
             if(move_uploaded_file($_FILES['audioFile']['tmp_name'][$i],$audio_path)){
                 echo "L'upload du titre numéro ".($i+1)." a réussi.<br>";
@@ -61,17 +61,17 @@ switch(isset($_POST)):
         include('../Models/get_music.php');
         $row = $stmt->fetchAll();
         if(!empty($row)){
-            echo '<div class ="parent" style="text-align:center;height:200px">';
+            echo '<div class ="parent" style="text-align:center;height:250px;width:300px">';
             echo '<center>Playlist en cours :</center>';
 
-        echo "<div class='child' style='float:right; margin-top: -32%; max-height:100%;overflow-y:scroll'>";
+        echo "<div class='child' style='float:right;width:95%; margin-top:0; max-height:100%;overflow-y:scroll'>";
         for($i=0; $i< count($row); $i++){
-            if(!empty($row[$i]['music_path'])){
-                echo "<img src='".$row[$i]['cover']."' height='50px' width ='50px'><a href='file:///".$row[$i]['music_path']."'>".$row[$i]['title']."</a><br>";
+            if(!empty($row[$i]['source'])){
+                echo "<br><img src='".$row[$i]['cover']."' height='30px' width ='30px'><a href='".$row[$i]['source']."'>".$row[$i]['titre']."</a> ".$row[$i]['name']."<br>";
             }
         }
-        echo '<div><p><form method="POST" action="">
-        <button type="submit" accesskey="n" style="margin-bottom:20px;margin-top:-20px;">Nouvelle liste aléatoire</button></form></p></div>';
+        echo '<div style="margin-left:50px"><center><p><form method="POST" action="">
+        <button type="submit" accesskey="n" style="margin-bottom:20px;margin-top:-20px;margin-right:+50px">Nouvelle liste aléatoire</button></form></p></center></div>';
         echo "</div>";
         echo "</div>";
         }
--- Player/Models/get_music.php
+++ Player/Models/get_music.php
@@ -1,8 +1,11 @@
 <?php
-    $query = 
-    "SELECT
-    title, featuring, music_path
-    FROM musics
+    $query = "
+    SELECT albums.album_cover as cover, musics.title as titre, artists.artist_name as name, musics.featuring as featuring, musics.music_path as source, albums.album_name AS album 
+    FROM musics 
+    JOIN albums 
+    JOIN artists 
+    ON artists.id_artist = albums.artist_id 
+    AND musics.artist = artists.id_artist
     ORDER BY RAND()";
         $query_params = null;
         try {
