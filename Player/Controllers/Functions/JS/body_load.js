f7dcb9c2e23ea735784a6a3d19dba43bf1058e1f
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-07 08:57:51 +0200

    forgot this one

--- Player/Controllers/Functions/JS/body_load.js
+++ Player/Controllers/Functions/JS/body_load.js
@@ -1,6 +1,6 @@
 function body_load() {
     $(window).load(function () {
-        // Evite un affichage dégueulasse quand la page se charge /
+        /* Evite un affichage dégueulasse quand la page se charge */
         $("body").fadeIn(100);
         $("div").fadeIn(100);
     });
