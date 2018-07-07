5a2afba89479d1056530c2334f72982b53c7c34b
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-07 10:10:23 +0200

    rofl

--- Models/db_connect.php
+++ Models/db_connect.php
@@ -1,11 +1,9 @@
 <?php
-<<<<<<< HEAD
         /* Module connexion à la bdd */
-
     $db_username = "root";
     $db_password = "";
     $db_host = "localhost";
-    $db_name = "veterinaires";
+    $db_name = "mystudio";
     $db_options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
 
           try {
@@ -16,12 +14,4 @@
 
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-=======
-function db_connect() {
-$bdd =new PDO('mysql:host=localhost;dbname=mystudio;charset=UTF8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-
-return $bdd;
-}
->>>>>>> 402eb69428fef7da8faaa3f0cc3bc308c8831673
-
 ?>
