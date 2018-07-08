2e2b23423707b882bd3616a7dbdd008bf2c135c9
Sid-Athos <sa.bennaceur@gmail.com>
2018-07-08 20:25:44 +0200

    CSS done I guess

--- Player/Controllers/Functions/JS/body_load.js
+++ Player/Controllers/Functions/JS/body_load.js
@@ -1,7 +1,8 @@
-    function body_load() {
-        $(window).load(function () {
-            /* Evite un affichage dégueulasse quand la page se charge */
-            $("body").fadeIn(100);
-            $("div").fadeIn(100);
-        });
-    }
+function body_load() {
+    $(window).ready(function () {
+        /* Evite un affichage dégueulasse quand la page se charge */
+        $("body").fadeIn(100);
+        $("body").CSS("background-color","hsla(199, 19%, 29%, 0.815)")
+        $("div").fadeIn(100);
+    });
+}
--- Player/Views/admin_options.php
+++ Player/Views/admin_options.php
@@ -7,16 +7,10 @@
     <title>MyStudio, admin</title>
     <meta content='http-equiv' content-type='text/html'/>
     <meta charset="UTF-8">
-    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
-    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-      <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-    <script type="text/javascript" src="../Controllers/Functions/JS/img_preview.js"></script>
-    <script type="text/javascript" src="../Controllers/Functions/JS/body_load.js"></script>
-    <script type="text/javascript" src="../Controllers/Functions/JS/search.js"></script>
     <link type="text/CSS" rel="stylesheet" href="../Player/Views/CSS/stylesheet.css">
-
+    <script type="text/javascript" src="../Controllers/Functions/JS/body_load.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+    
 </head>
 <body style="width:98%">
     
@@ -24,37 +18,37 @@
         <tr style="width:100%;height:60px">
             <td><a href="../Player/index.php?page=administration">
             <img src="../Player/Views/icons/icons8-cat-profile-96.svg" alt="Kitten" width="50px" height ="50px" class ="kitten_icon"></a>
-            <div class="hello"> Bonjour Admin [id]  Nous sommes le <?php echo $actual_date; ?>. </div></td>
+            <div class="hello"><span class="hello_text">Bonjour Admin [id]  Nous sommes le <?php echo $actual_date; ?>.</span> </div></td>
 
             <td style="text-align:left">
-            <div id="show_hide" style="margin-top:-46px;margin-left:-85px;position:absolute">
-                <div class="search_form" style="position:relative;padding-right:15px">
-                <form action="../Player/index.php?page=administration" id="target"name="search_form" method="POST">
-                <input type="search" autofocus id="search" class = "search" results=5 required style="border-radius:2px;background:color=white"name="search" placeholder="Rechercher un artiste..."/>
-</form>    
+                <div id="show_hide" style="margin-top:-46px;margin-left:-85px;position:absolute">
+                    <div class="search_form" style="position:relative;padding-right:15px">
+                        <form action="../Player/index.php?page=administration" id="target"name="search_form" method="POST">
+                        <input type="search" autofocus id="search" class = "search" results="5" required style="border-radius:2px;background:color=white"name="search" placeholder="Rechercher un artiste..."/>
+                    </form>    
             </div>
-            </div>
-            <div id="show_hide" style="margin-top:-20px;position:absolute">
-                <a href="../Player/index.php?page=administration">
-                <img src="../Player/Views/icons/cogs-solid.svg" alt="Kitten" width="40px" height ="40px" style="margin-left:90px" class ="kitten_icon"></a>
-                <a href="../Player/index.php?page=administration">
-                <img src="../Player/Views/icons/address-book-solid.svg" alt="Kitten" width="35px" height ="35px" class ="kitten_icon"></a>
-            </div>
-            </td>
-        </tr>
-        <tr style="height:10%">
-            <td class="body_zone"style="text-align:center;float:middle"><div class="corpse">Lorem Ipsum Dolor Sit Amet</div></td>
-            <td class="options_zone" colspan ="2"rowspan="2"style="text-align:center;width:12%;height:auto">
-            
-                <form action="../Player/index.php?page=administration" name="options" method="POST">
-                    <ul ss-container style="padding:5px;margin-top:5px">
-                        <li><div class="options_choice">Vos options :</div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="add_style" value="Ajouter un Genre"></div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="add_lyrics" value="Ajouter des Paroles"></div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="add_artist" value="Nouvel Artiste"></div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="add_album" value="Nouvel Album"></div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="remove_album" value="Supprimer un Album"></div></li>
-                        <li><div class="button_choice"><input class = "button" type="submit" name="remove_artist" value="Supprimer un Artiste"></div></li>
+        </div>
+        <div id="show_hide" style="margin-top:-20px;position:absolute">
+            <a href="../Player/index.php?page=administration">
+            <img src="../Player/Views/icons/cogs-solid.svg" alt="Kitten" width="40px" height ="40px" style="margin-left:90px" class ="kitten_icon"></a>
+            <a href="../Player/index.php?page=administration">
+            <img src="../Player/Views/icons/address-book-solid.svg" alt="Kitten" width="35px" height ="35px" class ="kitten_icon"></a>
+        </div>
+    </td>
+</tr>
+<tr style="height:10%">
+    <td class="body_zone"style="text-align:center;float:middle"><div class="corpse">Lorem Ipsum Dolor Sit Amet</div></td>
+    <td class="options_zone" colspan ="2"rowspan="2"style="text-align:center;width:12%;height:auto">
+        
+        <form action="../Player/index.php?page=administration" name="options" method="POST">
+        <ul ss-container style="padding:5px;margin-top:5px">
+            <li><div class="options_choice">Vos options :</div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="add_style" value="Ajouter un Genre"></div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="add_lyrics" value="Ajouter des Paroles"></div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="add_artist" value="Nouvel Artiste"></div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="add_album" value="Nouvel Album"></div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="remove_album" value="Supprimer un Album"></div></li>
+            <li><div class="button_choice"><input class = "button" type="submit" name="remove_artist" value="Supprimer un Artiste"></div></li>
                         <li><div class="button_choice"><input class = "button" type="submit" name="suppress_music" value="Supprimer une musique"></div></li>
                     </ul>
                 </form>
@@ -62,8 +56,8 @@
         </tr>
         <tr>
             <td style="text-align:center">Petites infos
-            </td>
-        </tr>
+                </td>
+            </tr>
         <tfoot class="footer_zone" style="width:101.45%;height:10%;margin-left:-15px">
             <tr>
                 <td colspan="2"style="text-align:center">
@@ -78,5 +72,12 @@
 
 <script>
     search();
-         
+    body_load();
 </script>
+
+<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
+<script type="text/javascript" src="../Controllers/Functions/JS/img_preview.js"></script>
+<script type="text/javascript" src="../Controllers/Functions/JS/search.js"></script>
